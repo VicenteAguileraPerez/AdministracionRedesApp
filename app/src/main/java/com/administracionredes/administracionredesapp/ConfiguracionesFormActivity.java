112ea159e752010fffcef53bea9dc668e236f946
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-03-09 13:09:19 -0600

    [ADD] activity de ConfiguracionesFormActivity

---
+++ app/src/main/java/com/administracionredes/administracionredesapp/ConfiguracionesFormActivity.java
@@ -0,0 +1,15 @@
+package com.administracionredes.administracionredesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracionesFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuraciones_form);
+        //Edgar Manuel
+    }
+}
