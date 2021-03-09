93e99ab6f1f7b749ed49d7d3bd8c87f5a40b1fae
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-03-09 13:02:00 -0600

    [ADD] activity de LocalizacionFormActivity

---
+++ app/src/main/java/com/administracionredes/administracionredesapp/LocalizacionFormActivity.java
@@ -0,0 +1,15 @@
+package com.administracionredes.administracionredesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalizacionFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localizacion_form);
+        //Edgar Manuel
+    }
+}
