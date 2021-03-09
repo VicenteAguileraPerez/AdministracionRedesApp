2fe310ee9f3ea6dd9247324473390b5b09935dbd
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-03-09 11:21:45 -0600

    [ADD] activity fallas form

--- app/src/main/java/com/administracionredes/administracionredesapp/FallasFormActivity.java
+++ app/src/main/java/com/administracionredes/administracionredesapp/FallasFormActivity.java
@@ -10,6 +10,6 @@ public class FallasFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fallas_form);
-        //Edgar Manuel
+
     }
 }
