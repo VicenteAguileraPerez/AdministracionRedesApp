0e6cbca607bec5222cfe132751b49c52109fbcf5
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-03-09 13:05:54 -0600

    [ADD] activity de InventarioFormActivity

---
+++ app/src/main/java/com/administracionredes/administracionredesapp/InventarioFormActivity.java
@@ -0,0 +1,15 @@
+package com.administracionredes.administracionredesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InventarioFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario_form);
+        //Edgar Manuel
+    }
+}
