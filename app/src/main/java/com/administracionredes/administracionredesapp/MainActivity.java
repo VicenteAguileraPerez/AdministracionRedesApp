e86646301baa244ced09aea34cf060875a926e82
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-03-09 20:10:55 -0600

    [ADD] backend to connect to firebase

--- app/src/main/java/com/administracionredes/administracionredesapp/MainActivity.java
+++ app/src/main/java/com/administracionredes/administracionredesapp/MainActivity.java
@@ -3,13 +3,64 @@ package com.administracionredes.administracionredesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private TextInputLayout textInputLayout_nombre;
+    private TextInputLayout textInputLayout_observaciones;
+    private MaterialButton materialButton_agregar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Edgar Manuel
+        setTitle("Administrador de redes");
+
+        textInputLayout_nombre = findViewById(R.id.textInputLayout_nombre);
+        textInputLayout_observaciones = findViewById(R.id.textInputLayout_observaciones);
+        materialButton_agregar = findViewById(R.id.button_agregar);
+
+
+        materialButton_agregar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String nombre = textInputLayout_nombre.getEditText().getText().toString();
+                String observaciones =textInputLayout_observaciones.getEditText().getText().toString();
+                if(evaluarDatos(nombre,textInputLayout_nombre) && evaluarDatos(observaciones,textInputLayout_observaciones))
+                {
+
+                    ///conexion a firebase
+                    String datos[] = {nombre,observaciones};
+                    Snackbar.make(view,"Datos validos",Snackbar.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Snackbar.make(view,"Datos invalidos",Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public boolean evaluarDatos(String valor,  TextInputLayout textInputLayout)
+    {
+        if(!valor.equals(""))
+        {
+            return true;
+        }
+        else
+        {
+            textInputLayout.setError("Campo vacío");
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/com/administracionredes/administracionredesapp/helpers/Data.java
@@ -0,0 +1,10 @@
+package com.administracionredes.administracionredesapp.helpers;
+
+import java.util.ArrayList;
+
+public interface Data
+{
+    public default void arrayList(ArrayList<Object> data) {
+
+    }
+}
