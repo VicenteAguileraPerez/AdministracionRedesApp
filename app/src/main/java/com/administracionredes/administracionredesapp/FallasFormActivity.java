7858e0d13ed0188255fe4b442056fc33c66fd17e
manuel <recio-manuel@outlook.com>
2021-03-10 09:24:49 -0600

    [ADD] mande llamar el metodo que obtiene los datos

--- app/src/main/java/com/administracionredes/administracionredesapp/FallasFormActivity.java
+++ app/src/main/java/com/administracionredes/administracionredesapp/FallasFormActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.administracionredes.administracionredesapp.helpers.Collections;
+import com.administracionredes.administracionredesapp.helpers.StaticHelper;
 import com.administracionredes.administracionredesapp.helpers.Status;
 import com.administracionredes.administracionredesapp.models.Fallas;
+import com.administracionredes.administracionredesapp.models.Inventario;
+import com.administracionredes.administracionredesapp.services.FirebaseHelper;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
@@ -30,7 +34,7 @@ public class FallasFormActivity extends AppCompatActivity implements Status {
         textInputLayout__tipo_de_falla = findViewById(R.id.textInputLayout_tipo_de_falla);
         textInputLayout_observaciones = findViewById(R.id.textInputLayout_observaciones);
         materialButton_agregar = findViewById(R.id.button_agregar);
-
+        getDatos();
         materialButton_agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,14 +45,20 @@ public class FallasFormActivity extends AppCompatActivity implements Status {
                 if (evaluarDatos(guia, textInputLayout_guia) && evaluarDatos(dispositivo, textInputLayout_dispositivo) && evaluarDatos(tipo_de_falla, textInputLayout__tipo_de_falla) && evaluarDatos(observaciones, textInputLayout_observaciones)) {
                     //Conexión exitosa
                     String datos[] = {guia, dispositivo, tipo_de_falla, observaciones};
-                    Snackbar.make(view, "Datos validos", Snackbar.LENGTH_SHORT).show();
+                    if (getIntent().getBooleanExtra("dato", false)) {
+                        //Edición e Firebase
+                        new FirebaseHelper().editar(FallasFormActivity.this::status, Collections.FALLAS.toString(), fallas.getId(), StaticHelper.FALLASKEYS, datos);
+                    } else {
+                        //Agregación en Firebase
+                        new FirebaseHelper().add(FallasFormActivity.this::status, Collections.FALLAS.toString(), StaticHelper.FALLASKEYS, datos);
+                    }
+                    Snackbar.make(view, "Datos válidos", Snackbar.LENGTH_SHORT).show();
                 } else {
-                    //Conexión NO exitosa
-                    Snackbar.make(view, "Datos invalidos", Snackbar.LENGTH_SHORT).show();
+                    //Conexión
+                    Snackbar.make(view, "Datos inválidos", Snackbar.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
 
     public boolean evaluarDatos(String valor, TextInputLayout textInputLayout) {
@@ -61,12 +71,18 @@ public class FallasFormActivity extends AppCompatActivity implements Status {
     }
 
     public void getDatos() {
-        if (getIntent().getBooleanExtra("Fallas", false)) {
+        if (getIntent().getBooleanExtra("dato", false)) {
+            fallas = (Fallas) getIntent().getSerializableExtra("Fallas");
+            textInputLayout_guia.getEditText().setText(fallas.getGuia());
+            textInputLayout_dispositivo.getEditText().setText(fallas.getDispositivo());
+            textInputLayout__tipo_de_falla.getEditText().setText(fallas.getTipo_de_falla());
+            textInputLayout_observaciones.getEditText().setText(fallas.getObservaciones());
+            materialButton_agregar.setText("Editar Falla");
         }
     }
 
     @Override
     public void status(String mensaje) {
-
+        Toast.makeText(FallasFormActivity.this, mensaje, Toast.LENGTH_SHORT).show();
     }
 }
