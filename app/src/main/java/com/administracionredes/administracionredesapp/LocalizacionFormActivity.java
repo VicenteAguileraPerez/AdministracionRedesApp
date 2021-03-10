dc9a8720d3fe9f8dd4fc723ca7d7873ad53788c8
AntonioGarcialll <62670455+AntonioGarcialll@users.noreply.github.com>
2021-03-10 09:57:00 -0600

    [ADD] Implementé el método eliminar y la interfaz Data.

--- app/src/main/java/com/administracionredes/administracionredesapp/LocalizacionFormActivity.java
+++ app/src/main/java/com/administracionredes/administracionredesapp/LocalizacionFormActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.administracionredes.administracionredesapp.helpers.Collections;
+import com.administracionredes.administracionredesapp.helpers.StaticHelper;
+import com.administracionredes.administracionredesapp.helpers.Status;
+import com.administracionredes.administracionredesapp.models.Inventario;
+import com.administracionredes.administracionredesapp.models.Localizacion;
+import com.administracionredes.administracionredesapp.services.FirebaseHelper;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class LocalizacionFormActivity extends AppCompatActivity {
+public class LocalizacionFormActivity extends AppCompatActivity implements Status {
     private TextInputLayout textInputLayout_nombre;
     private TextInputLayout textInputLayout_tipo;
     private TextInputLayout textInputLayout_localizacion;
     private TextInputLayout textInputLayout_status;
     private TextInputLayout textInputLayout_observaciones;
+
     private MaterialButton materialButton_agregar;
+    Localizacion localizacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +42,27 @@ public class LocalizacionFormActivity extends AppCompatActivity {
         textInputLayout_status = findViewById(R.id.textInputLayout_status);
         textInputLayout_observaciones = findViewById(R.id.textInputLayout_observaciones);
         materialButton_agregar = findViewById(R.id.button_agregar);
-
+        getDatos();
         materialButton_agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String nombre = textInputLayout_nombre.getEditText().getText().toString();
                 String tipo = textInputLayout_tipo.getEditText().getText().toString();
-                String localizacion = textInputLayout_localizacion.getEditText().getText().toString();
+                String localizacions = textInputLayout_localizacion.getEditText().getText().toString();
                 String status = textInputLayout_status.getEditText().getText().toString();
                 String observaciones = textInputLayout_observaciones.getEditText().getText().toString();
 
-                if (evaluarDatos(nombre, textInputLayout_nombre) && evaluarDatos(tipo, textInputLayout_tipo) && evaluarDatos(localizacion, textInputLayout_localizacion) &&
+                if (evaluarDatos(nombre, textInputLayout_nombre) && evaluarDatos(tipo, textInputLayout_tipo) && evaluarDatos(localizacions, textInputLayout_localizacion) &&
                         evaluarDatos(status, textInputLayout_status) && evaluarDatos(observaciones, textInputLayout_observaciones)) {
                     //Conexión exitosa
-                    String datos[] = {nombre, tipo, localizacion, status, observaciones};
+                    String datos[] = {nombre, tipo, localizacions, status, observaciones};
+                    if (getIntent().getBooleanExtra("dato", false)) {
+                        //edicion e firebase
+                        new FirebaseHelper().editar(LocalizacionFormActivity.this::status, Collections.LOCALIZACION.toString(), localizacion.getId(), StaticHelper.LOCALIZACIONKEYS, datos);
+                    } else {
+                        //agregacion en firebase
+                        new FirebaseHelper().add(LocalizacionFormActivity.this::status, Collections.LOCALIZACION.toString(), StaticHelper.LOCALIZACIONKEYS, datos);
+                    }
                     Snackbar.make(view, "Datos válidos", Snackbar.LENGTH_SHORT).show();
                 } else {
                     //Conexión
@@ -64,5 +80,23 @@ public class LocalizacionFormActivity extends AppCompatActivity {
             textInputLayout.setError("Campo vacío");
             return false;
         }
+    }
+
+    public void getDatos() {
+        if (getIntent().getBooleanExtra("dato", false)) {
+            localizacion = (Localizacion) getIntent().getSerializableExtra("Localizacion");
+            textInputLayout_nombre.getEditText().setText(localizacion.getNombre());
+            textInputLayout_observaciones.getEditText().setText(localizacion.getObservaciones());
+            textInputLayout_status.getEditText().setText(localizacion.getEstatus());
+            textInputLayout_tipo.getEditText().setText(localizacion.getTipo());
+            textInputLayout_localizacion.getEditText().setText(localizacion.getLocalizacion());
+            materialButton_agregar.setText("Editar Localizacion");
+        }
+    }
+
+    @Override
+    public void status(String mensaje) {
+        Toast.makeText(LocalizacionFormActivity.this, mensaje, Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/administracionredes/administracionredesapp/LocalizacionListaActivity.java
+++ app/src/main/java/com/administracionredes/administracionredesapp/LocalizacionListaActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.administracionredes.administracionredesapp.adapters.AdapterItemLocalizacion;
+import com.administracionredes.administracionredesapp.helpers.Collections;
+import com.administracionredes.administracionredesapp.helpers.Data;
+import com.administracionredes.administracionredesapp.helpers.Status;
 import com.administracionredes.administracionredesapp.models.Localizacion;
+import com.administracionredes.administracionredesapp.services.FirebaseHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
 
-public class LocalizacionListaActivity extends AppCompatActivity {
+public class LocalizacionListaActivity extends AppCompatActivity implements Data, Status {
     private FloatingActionButton floatingActionButton;
     private ArrayList<Localizacion> arrayList;
     private RecyclerView recyclerView;
@@ -41,7 +48,7 @@ public class LocalizacionListaActivity extends AppCompatActivity {
     }
 
     public void llenar(ArrayList<Localizacion> localizaciones) {
-        recyclerView.setAdapter(new AdapterItemLocalizacion(localizaciones, LocalizacionListaActivity.this));
+        recyclerView.setAdapter(new AdapterItemLocalizacion(localizaciones, LocalizacionListaActivity.this,LocalizacionListaActivity.this));
         StaggeredGridLayoutManager mLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(mLayoutManager);
     }
@@ -52,5 +59,26 @@ public class LocalizacionListaActivity extends AppCompatActivity {
         arrayList.add(new Localizacion(UUID.randomUUID().toString(), "1234AB", "Modem", "Se quemo", "ps esta quemao no ves","hola"));
         arrayList.add(new Localizacion(UUID.randomUUID().toString(), "1234AB", "Modem", "Se quemo", "ps esta quemao no ves","hola"));
         llenar(arrayList);
+    }
+
+    @Override
+    public void arrayList(ArrayList<Object> data) {
+        arrayList = new ArrayList<>();
+        for (Object obj :data) {
+            Localizacion loc = (Localizacion)obj;
+            arrayList.add(loc);
+        }
+        llenar(arrayList);
+    }
+
+    @Override
+    public void id(String id) {
+        new FirebaseHelper().eliminar(LocalizacionListaActivity.this::status, Collections.LOCALIZACION.toString(),id);
+
+    }
+
+    @Override
+    public void status(String mensaje) {
+        Toast.makeText(LocalizacionListaActivity.this, mensaje,Toast.LENGTH_SHORT).show();
     }
 }
