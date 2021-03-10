01ee527145bd72dbcdae100a8aaf2568da432ae0
AntonioGarcialll <62670455+AntonioGarcialll@users.noreply.github.com>
2021-03-09 22:06:15 -0600

    [ADD] Agregué textfield de Nombre de dispositivo

---
+++ app/src/main/java/com/administracionredes/administracionredesapp/ListaActivity.java
@@ -0,0 +1,54 @@
+package com.administracionredes.administracionredesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class ListaActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
+    private ArrayList<Localizacion> arrayList;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+        recyclerView = findViewById(R.id.lista_localizaciones);
+        floatingActionButton = findViewById(R.id.fab_mas);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirActividad();
+            }
+        });
+        arrayList();
+    }
+
+    public void abrirActividad() {
+        Intent intent = new Intent(this, LocalizacionFormActivity.class);
+        startActivity(intent);
+    }
+
+    public void llenar(ArrayList<Localizacion> localizaciones) {
+        recyclerView.setAdapter(new AdapterItemLocalizacion(localizaciones, ListaActivity.this));
+        StaggeredGridLayoutManager mLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+    }
+
+    public void arrayList() {
+        arrayList = new ArrayList<Localizacion>();
+        arrayList.add(new Localizacion(UUID.randomUUID().toString(), "1234AB", "Modem", "Se quemo", "ps esta quemao no ves","hola"));
+        arrayList.add(new Localizacion(UUID.randomUUID().toString(), "1234AB", "Modem", "Se quemo", "ps esta quemao no ves","hola"));
+        arrayList.add(new Localizacion(UUID.randomUUID().toString(), "1234AB", "Modem", "Se quemo", "ps esta quemao no ves","hola"));
+        llenar(arrayList);
+    }
+}
---
+++ app/src/main/java/com/administracionredes/administracionredesapp/Localizacion.java
@@ -0,0 +1,65 @@
+package com.administracionredes.administracionredesapp;
+
+import java.io.Serializable;
+
+public class Localizacion implements Serializable
+{
+    private String id, nombre, tipo, localizacion, estatus, observaciones;
+
+    public Localizacion(String id, String nombre, String tipo, String localizacion, String estatus, String observaciones) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.localizacion = localizacion;
+        this.estatus = estatus;
+        this.observaciones = observaciones;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+}
