21dc9948294e4ab7cfe3fb48c3f2e3bc6a143f13
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-03-11 11:46:10 -0600

    [ADD] apk

--- app/src/main/java/com/administracionredes/administracionredesapp/MainActivity.java
+++ app/src/main/java/com/administracionredes/administracionredesapp/MainActivity.java
@@ -1,64 +1,40 @@
 package com.administracionredes.administracionredesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
 import android.os.Bundle;
 import android.view.View;
+
+import com.administracionredes.administracionredesapp.adapters.AdapterItemTopic;
+import com.administracionredes.administracionredesapp.helpers.Collections;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity
 {
-
-    private TextInputLayout textInputLayout_nombre;
-    private TextInputLayout textInputLayout_observaciones;
-    private MaterialButton materialButton_agregar;
+    private RecyclerView recyclerView_topic;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Administrador de redes");
-
-        textInputLayout_nombre = findViewById(R.id.textInputLayout_nombre);
-        textInputLayout_observaciones = findViewById(R.id.textInputLayout_observaciones);
-        materialButton_agregar = findViewById(R.id.button_agregar);
-
-
-        materialButton_agregar.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                String nombre = textInputLayout_nombre.getEditText().getText().toString();
-                String observaciones =textInputLayout_observaciones.getEditText().getText().toString();
-                if(evaluarDatos(nombre,textInputLayout_nombre) && evaluarDatos(observaciones,textInputLayout_observaciones))
-                {
-
-                    ///conexion a firebase
-                    String datos[] = {nombre,observaciones};
-                    Snackbar.make(view,"Datos validos",Snackbar.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Snackbar.make(view,"Datos invalidos",Snackbar.LENGTH_SHORT).show();
-                }
-            }
-        });
+        recyclerView_topic = findViewById(R.id.recyclerView_topic);
+        llenar();
 
     }
 
-    public boolean evaluarDatos(String valor,  TextInputLayout textInputLayout)
-    {
-        if(!valor.equals(""))
-        {
-            return true;
-        }
-        else
-        {
-            textInputLayout.setError("Campo vacío");
-            return false;
-        }
+    public void llenar() {
+        ArrayList<String> topic = new ArrayList<>(Arrays.asList(Collections.FALLAS.toString(), Collections.LOCALIZACION.toString(), Collections.INVENTARIO.toString(), Collections.CONFIGURACION.toString()));
+        recyclerView_topic.setAdapter(new AdapterItemTopic(topic, MainActivity.this));
+        StaggeredGridLayoutManager mLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView_topic.setLayoutManager(mLayoutManager);
     }
+
 }
